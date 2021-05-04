08603ef5c95817f3209f42f3a2b9eff3d8c16eef
nilochristian <nilochristian@gmail.com>
2021-05-04 19:56:28 -0300

    update code functionally

--- piro_rx.py
+++ piro_rx.py
@@ -5,9 +5,16 @@ class OlaMundo(object):
     def ola_mundo(self,name):
         return f'Ola Mundo para {name}\n'
 
-daemon = Pyro5.api.Daemon()
-ns = Pyro5.api.locate_ns()
-uri = daemon.register(OlaMundo)
-print("Ready. Object uri =", uri)
-ns.register("HelloWorld",uri)
-daemon.requestLoop()
+
+Pyro5.api.Daemon.serveSimple({
+    OlaMundo: 'HelloWord',
+}, host="localhost", port=1234, ns=False)
+
+#*
+#daemon = Pyro5.server.Daemon()
+#ns = Pyro5.api.locate_ns()
+#uri = daemon.register(OlaMundo)
+#print("Ready. Object uri =", uri)
+#name_server = "HelloWorld"
+#ns.register(name_server,uri,True)
+#daemon.requestLoop()
--- piro_tx.py
+++ piro_tx.py
@@ -2,5 +2,13 @@ import Pyro5.api
 
 #uri = "PYRO:obj_829bcf9ace7547ba9488f76437a72030@localhost:62022"
 #o = Pyro5.api.Proxy(uri)
-o = Pyro5.api.Proxy("PYRONAME:HelloWorld")
-print(o.ola_mundo(f'Fulano'))
+#o = Pyro5.api.Proxy("PYRONAME:HelloWorld")
+#print(o.ola_mundo(f'Fulano'))
+
+ip = "localhost"
+port = 1234
+
+o = Pyro5.api.Proxy(
+    f'PYRO:HelloWord@{ip}:{port}'
+    )
+print(o.ola_mundo("Fulano"))
